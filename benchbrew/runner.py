3f0d3080b0fbed6c67e7ebe08bdd5ccbd2c7fafb
Buck <buck@singularitybrewing.co>
2026-08-09 19:21:18 +0000

    fix: HTTPError import

--- benchbrew/runner.py
+++ benchbrew/runner.py
@@ -14,6 +14,7 @@ import time
 import urllib.error
 import urllib.request
 from dataclasses import dataclass, field
+from urllib.error import HTTPError
 
 from .generator import Generator
 from .simulator import Simulator
